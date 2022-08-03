e6d81f72628cb2dc7dfe7b308b6cdc8385705b04
iceman_2946 <advaithkarthik9@gmail.com>
2022-08-03 12:17:52 -0700

    3rd day of programming in Python 2/2

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -13,22 +13,23 @@ if __name__ == '__main__':
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    
+    Radius=simpledialog.askinteger('title','What is the radius in pixels of the circle?')
     # Make a new turtle
-    
+    Terry=turtle.Turtle()
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
-
+    Terry.circle(Radius)
     # Call the turtle .penup() method
-
+    Terry.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    Terry.goto(51,-23)
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    Area=math.pi*Radius*Radius
     # Write the area of your circle using the turtle .write() method
     # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-
+    Terry.write(arg="area= "+ str(Area), move=True, align='left', font=('Arial',8,'normal'))
     # Hide your turtle
-
+    Terry.hideturtle()
     # Call turtle.done()
+    turtle.done()
--- _03_if_else/_3_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_3_tasty_tomato/tasty_tomato.py
@@ -1,19 +1,20 @@
-from tkinter import *
-import tkinter as tk
+from tkinter import messagebox, simpledialog, Tk
+
 
 window_width = 600
 window_height = 600
 
-root = tk.Tk()
+root = Tk()
 
-canvas = tk.Canvas(root, width=window_width, height=window_height, bg="#DDDDDD")
+canvas = Canvas(root, width=window_width, height=window_height, bg="#DDDDDD")
 canvas.grid()
 
 # 1. Ask the user what color tomato they would like and save their response
 #    You can give them up to three choices
-
-
+answer=simpledialog.askstring('title','What color tomato would you like? Blue,red or green?')
+print(answer)
 # 2. Use if-else statements to draw the tomato in the color that they chose
+if answer=blue:
 #    You can modify the code below or draw your own tomato
 canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
 canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
